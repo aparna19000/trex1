ffce982dc0f7de42cbdddb5b87522dd427de60af
aparna19000 <54104999+aparna19000@users.noreply.github.com>
2020-04-13 15:30:13 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
-var PLAY = 1;
-var END = 0;
-var gameState = PLAY;
+var PLAY = 1
+var END = 0
+var gameState = PLAY
 
-var trex, trex_running, trex_collided;
-var ground, invisibleGround, groundImage;
+var trex, trex_running, trex_collided
+var ground, invisibleGround, groundImage
 
-var cloudsGroup, cloudImage;
+var cloudsGroup, cloudImage
 var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
 
 var score=0;
